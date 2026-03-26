da6928fd2e591af27817e51c19cd9b54e3c72608
valenFu <valentinchufunes4@gmail.com>
2026-03-26 12:27:52 -0300

    fix: login sin localhost

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", async (e) => {
   const password = document.getElementById("password").value;
 
   try {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("/api/auth/login", { // FIX
       method: "POST",
       headers: {
         "Content-Type": "application/json"
